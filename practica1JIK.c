ce6c118c406d6a2ae8b578dba58d3040e82e9cc2
JeremyAlexis132 <jeremyalexis_132@comunidad.unam.mx>
2025-03-10 21:02:19 -0600

    Created Practica1 documents Created multiMatrix 6 cases

---
+++ practica1JIK.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+//cambiar esta linea de acuerdo al tamaño de la matriz
+#define MAX 100
+
+int main() {
+    int matrizA[MAX][MAX], matrizB[MAX][MAX], matrizC[MAX][MAX];
+    int i=0, j=0, k=0;
+    for(i=0; i<MAX; i++) {
+        for(j=0; j<MAX; j++) {
+            matrizA[i][j] = 1;
+            matrizB[i][j] = 2;
+        }
+    }
+    clock_t start = clock();
+    for(j=0; j<MAX; j++) {
+        for(i=0; i<MAX; i++) {
+            for(k=0; k<MAX; k++) {
+                matrizC[i][j] += matrizA[i][k] * matrizB[k][j];
+            }
+        }
+    }
+    clock_t end = clock();
+    double time = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("Tiempo de ejecucion: %f\n", time);
+    return 0;
+}
